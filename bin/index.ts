5cec890cfa07646dab8fd0b06b2e3d94795348a8
Sir_shane <uogbulafor@vasconsolutions.com>
2022-06-21 10:08:36 +0100

    config typescript

---
+++ bin/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const yargs = require('yargs');
+const path = require('path');
+const fs = require('fs');
+
+const options = yargs
+  .usage('Usage: --i <input file>')
+  .usage('Usage: --0 <output file>')
+  .options('i', {
+    alias: 'input',
+    describe: 'input file log',
+    type: 'file',
+    demandOption: true,
+  })
+  .options('o', {
+    alias: 'output',
+    describe: 'output file',
+    type: 'file',
+    demandOption: true,
+  }).argv;
+
+const inputData = path.join(__dirname, '../file', options.input);
+const data = fs.readFileSync(inputData, 'utf8');
+// convert to array
+const newDataArray = data.split('\n');
+newDataArray.forEach(data => {
+  const [timeStamp, logLevel, others] = data.split(' - ');
+  const {transactionId, err, ...details} = JSON.parse(others);
+  const newObject = {timeStamp, logLevel, transactionId, err: err ? err : ''};
+  console.log(newObject);
+});
+
+// class Logger {
+//   constructor() {}
+
+//   readData() {}
+// }
